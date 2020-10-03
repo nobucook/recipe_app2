4107a10ec9a5ffbb6e6c125c7b6d770ffdfa61f6
nobucook <tomo.nobu777@gmail.com>
2020-10-03 11:46:17 +0900

    modified db/seeds, db/fixture

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,15 @@
-# メインのサンプルユーザーを1人作成する
+# メインのサンプルユーザー（admin）を1人作成する
 User.create!(name:  "Admin User",
              email: "tomo.nobu777+admin@gmail.com",
-             password:              "foobar",
-             password_confirmation: "foobar",
+             password:              "unkotintin",
+             password_confirmation: "unkotintin",
              admin: true)
 
-#簡単ログイン用のユーザーを作成する。
-User.create!(name: "Guest User",
-                   email: "test@example.com",
-                   password: "unkotintin",
-                  password_confirmation: "unkotintin")
+#簡単ログイン用のユーザーを作成する。(開発環境用)
+# User.create!(name: "Guest User",
+#                    email: "test@example.com",
+#                    password: "foobar",
+#                   password_confirmation: "foobar")
 
 
 #Categoryを作成
@@ -19,30 +19,45 @@ categories.length.times do |n|
   Category.create!(name: name)
 end
   
-  # 追加のユーザーをまとめて生成する
-99.times do |n|
-  name  = Faker::Name.name
-  email = "example-#{n+1}@railstutorial.org"
-  password = "password"
-  User.create!(name:  name,
-               email: email,
-               password:              password,
-               password_confirmation: password)
-end
+  # 追加のユーザーをまとめて生成する（開発環境用）
+# 99.times do |n|
+#   name  = Faker::Name.name
+#   email = "example-#{n+1}@railstutorial.org"
+#   password = "password"
+#   User.create!(name:  name,
+#                email: email,
+#                password:              password,
+#                password_confirmation: password)
+# end
 
-users = User.order(:created_at).take(6)
-50.times do
-  title = "test test test test test"
-  about = "test test test test test test test test test test test test test test test test test test test test test"
-  users.each { |user| user.recipes.create!(title: title,
-                                            about: about) }
-end
+#開発環境用
+# users = User.order(:created_at).take(6)
+# 50.times do
+#   title = "Test Test Test"
+#   about = "test test test test test test test test test test test test test test test test test test test test test"
+#   ingre = "rice"
+#   amount = "1cup"
+#   how_to = "wash Rice"
+#   no = 1
+#   users.each { |user| user.recipes.create!(title: title,
+#                                             about: about,
+#                                           category_ids: [1],
+#                                           # image: open("#{Rails.roots}/db/fixtures/image1.jpg"),
+#                                           ingredients_attributes: {
+#                                             '0': {ingre: ingre, amount: amount}
+#                                           },
+#                                           instructions_attributes: {
+#                                             '0': {no: no, how_to: how_to}
+#                                           }
+#                                         ) 
+#                                         }
+# end
 
-
-user = User.first 
-user2 = User.second 
-liker = users[2..10]
-recipes = Recipe.all[2..10]
-recipes2 = Recipe.all[6..15]
-recipes.each{|liked| user.like(liked)}
-recipes2.each{|liked2| user2.like(liked2)}
+#開発環境用
+# user = User.first 
+# user2 = User.second 
+# liker = users[2..10]
+# recipes = Recipe.all[2..10]
+# recipes2 = Recipe.all[6..15]
+# recipes.each{|liked| user.like(liked)}
+# recipes2.each{|liked2| user2.like(liked2)}

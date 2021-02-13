ff19a05ff84bf25556739b39f2f6eb6183e7ec4b
nobucook <tomo.nobu777@gmail.com>
2021-02-13 15:48:59 +0900

    add ajax for liking

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,15 +2,23 @@ class LikesController < ApplicationController
   before_action :logged_in_user
 
   def create
-    recipe = Recipe.find(params[:liked_id])
-    current_user.like(recipe)
-    redirect_back(fallback_location: recipe)
+    @recipe = Recipe.find(params[:liked_id])
+    current_user.like(@recipe)
+    # redirect_back(fallback_location: @recipe)
+    respond_to do |format|
+      format.html {redirect_back fallback_location: @recipe}
+      format.js
+    end
   end
 
   def destroy
-    recipe = Like.find(params[:id]).liked
-    current_user.unlike(recipe)
-    redirect_back(fallback_location: recipe)
+    @recipe = Like.find(params[:id]).liked
+    current_user.unlike(@recipe)
+    # redirect_back(fallback_location: @recipe)
+    respond_to do |format|
+      format.html {redirect_back fallback_location: @recipe}
+      format.js
+    end
   end
-  
+
 end
